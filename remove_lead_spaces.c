c84ec86ad8b0e091448750ae9308caf2fc0a0028
Crying-Soul <grebnevegor2000@mail.ru>
2023-10-19 01:08:41 +0300

    file moved

--- remove_lead_spaces.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-
-void remove_lead_spaces(char* str) {
-        char* start = str;
-        while (*start && (*start == ' ' || *start == '\t' ||  *start == '\n')) {
-                start++;
-        }
-        memmove(str, start, strlen(start) + 1);
-}
