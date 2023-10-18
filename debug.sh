296730e53eb3f2a2c37a83495a753a77b46a9533
Crying-Soul <grebnevegor2000@mail.ru>
2023-10-19 01:06:36 +0300

    Update auto tests for folders & named args

--- debug.sh
+++ debug.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 
-if make; then
+if [ "$#" -eq 1 ]; then
+    test_file="$1"
+else
+
+    echo "Using default test's file..."
+    test_file="default"
+fi
+
+run_tests() {
     test_number=0
     current_test=""
     
@@ -17,16 +25,22 @@ if make; then
         else
             current_test="$current_test\n$line"
         fi
-    done < tests
+    done < "tests/$1"
     
     if [ -n "$current_test" ]; then
         test_number=$((test_number+1))
         echo -e "\033[31mRunning test $test_number\033[0m"
         echo -e "$current_test" | ./coursework
+        sleep 1.5
     fi
+}
+
+if make; then
+    run_tests "$test_file"
+    make clean
+    echo -e "\033[1;92m------------\033[0m"
+    echo -e "\033[1;92mTests ended!\033[0m"
+    echo -e "\033[1;92m------------\033[0m"
 else
-    echo "Ошибка компиляции программы."
+    echo "Compilation error..."
 fi
-
-make clean
-
